8cadc445a37fd8550771b28d96bbb054decf7f05
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2023-11-29 20:16:10 -0800

    updated the starter code, however front end is still not working as expected

--- Develop/server.js
+++ Develop/server.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded()); //helps with invalid chars
+app.use(express.urlencoded());
 
 app.get('/api/notes', (req, res) => {
   res.json(dbJson)
